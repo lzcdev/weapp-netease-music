95973e2425a74ecb617d57c09adf1fa698a632a9
lzcdev <lzc2134@gmail.com>
2019-05-23 16:48:42 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,24 +1,9 @@
-# CitySelectView
-二级城市选择，自定义view
+## 小程序版本网易云音乐
 
-![License MIT](https://go-shields.herokuapp.com/license-MIT-blue.png)
-![Platform info](http://img.shields.io/cocoapods/p/YTKKeyValueStore.svg?style=flat)
-## 实例
+## API
 
-![演示效果](http://7xt7mf.com1.z0.glb.clouddn.com/%E5%9F%8E%E5%B8%82%E9%80%89%E6%8B%A9.gif?imageMogr2/auto-orient/strip%7CimageView2/4/w/400)
+API 来自 [NeteaseCloudMusicApi](https://github.com/Binaryify/NeteaseCloudMusicApi)
 
-## 用法
+我把这个项目部署到自己服务器上了，配置的域名为 [http://netease.lzcdev.xyz/](http://netease.lzcdev.xyz/)
 
-导入CitySelectView
-
-```
-CitySelectView *cityView = [[CitySelectView alloc]initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-100) provinceFrame:CGRectMake(0, 0, 100, 300) cityFrame:CGRectMake(100, 0, [UIScreen mainScreen].bounds.size.width-100, 300)];
-    __weak typeof(self)weakSelf = self;
-    cityView.passCityBlock = ^(NSString *selectCity) {
-        // 赋值
-        [weakSelf.citySelectBtn setTitle:selectCity forState:UIControlStateNormal];
-    };
-    [self.view addSubview:cityView];
-```
-
-
+## 项目介绍
---
+++ utils/api.js
@@ -0,0 +1,69 @@
+const baseURL = 'https://shengwang.lujingkeji.com'
+
+const http = ({
+  url = '',
+  params = {},
+  ...other
+} = {}) => {
+  wx.showLoading({
+    title: '加载中...',
+  })
+  let time = Date.now()
+  console.log(`开始:${time}`)
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: getUrl(url),
+      data: params,
+      header: getHeader(),
+      ...other,
+      complete: (res) => {
+        wx.hideLoading()
+        console.log(`耗时：${Date.now() - time}`)
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          resolve(res.data)
+        } else {
+          reject(res)
+        }
+      }
+    })
+  })
+}
+
+const getUrl = url => {
+  if (url.indexOf('://') == -1) {
+    url = baseURL + url
+  }
+  return url
+}
+
+const getHeader = () => {
+  try {
+    var token = wx.getStorageSync('token')
+    if (token) {
+      return {
+        'token': token
+      }
+    }
+
+  } catch (e) {
+
+  }
+}
+
+module.exports = {
+  baseURL,
+  get(url, params = {}) {
+    return http({
+      url,
+      params,
+      method: 'get'
+    })
+  },
+  post(url, params = {}) {
+    return http({
+      url,
+      param,
+      method: 'post'
+    })
+  }
+}
